35adae13f31685211d836554b087c583776bce8a
shivaduth <71396105+shivaduth@users.noreply.github.com>
2022-02-01 19:24:44 +0530

    Add files via upload

---
+++ interface vs types Typescript exercises/Exercise1.ts
@@ -0,0 +1,30 @@
+ // Exercise1:
+    // Given the data, define the interface "User" and use it accordingly.
+ 
+
+    //type User = unknown;
+    interface User{
+        name:string;
+        age:number;
+        occupation:string;
+    }
+
+    const users: User[] = [
+        {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+        },
+        {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+        }
+    ];
+
+    function logPerson(user: User) {
+        console.log(` - ${user.name}, ${user.age}`);
+    }
+        
+    console.log('Users:');
+    users.forEach(logPerson);
